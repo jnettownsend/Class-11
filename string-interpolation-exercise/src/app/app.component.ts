49f3791ad9bc6099361124169cedabf2d09b12e5
Jeannette Townsend <111323478+jnettownsend@users.noreply.github.com>
2022-10-05 09:13:03 -0500

    String Interpolation Exercise
    Class 11, exercise 3

---
+++ string-interpolation-exercise/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'string-interpolation-exercise';
+
+  myName:string = 'Jeannette';
+  constructor(){
+
+  }
+
+}
+
+
